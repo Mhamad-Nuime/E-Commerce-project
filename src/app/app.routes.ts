dafaabd8fcd5f1953e9ed3a050b92ade0ce5d294
Muhamad Nuime <moesnua@gmail.com>
2024-04-07 15:20:04 +0300

    create the files of the products and carts components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { CartsComponent } from './carts/carts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'carts',
+    component: CartsComponent
+  }
+];
---
+++ src/app/components/carts/carts.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carts',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      carts works!
+    </p>
+  `,
+  styleUrl: './carts.component.css'
+})
+export class CartsComponent {
+
+}
