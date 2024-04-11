8ad5c6bba4ed8809425b525d0676383d32063ed5
Muhamad Nuime <moesnua@gmail.com>
2024-04-11 03:22:44 +0300

    Implement the load spinner and bind it to loaderService that relying on the interceptorService , Rejester the InterceptorService inside the AppConfig

---
+++ src/app/services/interceptor.service.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, finalize } from 'rxjs';
+import { LoaderService } from './loader.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InterceptorService implements HttpInterceptor {
+  constructor(){
+    console.log(`hi I'm interceptor instance`);
+  }
+  loaderService = inject(LoaderService);
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(`catch the request before be sending.....`)
+    this.loaderService.isLoad.next(true);
+    return next.handle(req).pipe(
+      finalize(
+        () => {
+          this.loaderService.isLoad.next(false);
+        }
+      )
+    )
+  }
+}
